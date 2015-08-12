a63ce16eb1108a214cbdf6ffa66abe1de638bb1b
ochoa95 <och14230@uvg.edu.gt>
2015-08-11 21:10:13 -0600

    Update DomblementeEnlazada.java

--- DomblementeEnlazada.java
+++ DomblementeEnlazada.java
@@ -1,40 +1,62 @@
-public class DoblementeEnlazada(){
-	protected count 
-	protected cabeza
-	protected cola
-	DobleNodo aux = List;
-	 
-	public E RemoveLast (){
+public class DoblementeEnlazada<E> implements SimpleEnlazada<E>{
+Nodo<E> head;
+int contador;
 
-		while (List.getNext()!=null){
-			List = List.getNext()
-			
-			if (List.getNext() != null){
-				List.getPrevious();
-				List.setNext(null);
-			}
-		}
-		return List
+public DoblementeEnlazada(){
+head=null; 
+contador=0;
+}
+
+public int size(){
+
+}
+
+public void addFirst (E value){
+
+	}
+
+public E removeFirst(){
+
 	}
 	
-	public void addLast(E value){
+public E getFirst(){
+
+	}
+	
+
+
+public void addLast (E value){
+	Nodo<E> temp=new Nodo<E>();
+	Nodo<E> temp2=new Nodo<E>();
+	temp.setValue(value);
+	if(head!=null)
+		{
+			Nodo<E> finger=head;
+			while (finger.getNext() != null){
+				finger.setNext(finger.getNext());
+				temp2 = finger.setNext(finger.getNext());
+			}
+			
+		finger.setNext(temp);
+		finger.setNext(finger.getNext());
+		finger.setPrevious(temp2);
 		
-		while (List.getNext()!=null){
-			List = List.getNext()
-			
-			if (List.getNext() != null){
-				lastV = List.getPrevious();
-				List.setPrevious(value);
-				List.setNext(lastV);
-				
-			}
+	} else head=temp;
+		contador++;
 		}
+	
+
+public E getLast(){
+
+}
+	
+public E removeLast(){
+		
 	}
 	
-	public E getLast(){
-		while (List.getNext()!=null){
-			List = List.getNext()
+public boolean contains(E value){
+	
 		}
-		return List
-	}
+					
+					
 }
